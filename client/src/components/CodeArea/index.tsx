ec7fe1016f0fe4761b0708b88f032b9eb26e1bd8
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2024-01-09 01:54:41 +0530

    Updated JoinedUsers Ui

--- client/src/components/CodeArea/index.tsx
+++ client/src/components/CodeArea/index.tsx
@@ -1,21 +1,32 @@
 import { useState } from 'react';
 import styles from './styles.module.css';
 
+interface IUsers {
+  usernames: string[];
+}
+
+const JoinedUsers = ({ usernames }: IUsers) => {
+  return (
+    <div className={styles['joinedUsers']}>
+      <div style={{ fontWeight: 'bold' }}>Live Users: </div>
+      {usernames.map((user, index) => {
+        return (
+          <div key={index + user} className={styles['userList']}>
+            {user}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const CodeArea = () => {
   const [code, setCode] = useState('');
   const usernames = ['User-1', 'User-2', 'User-3', 'User-4', 'User-5'];
 
   return (
     <div>
-      <div className={styles['joinedUsers']}>
-        {usernames.map((user, index) => {
-          return (
-            <div key={index + user} className={styles['userList']}>
-              {user}
-            </div>
-          );
-        })}
-      </div>
+      <JoinedUsers usernames={usernames} />
       <div className={styles['codeEditor']}>
         <textarea
           className={styles['codeArea']}
