cff2d52ea75df0a82e95c585e959b81e9e2e9ac4
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2024-01-10 01:36:59 +0530

    duplicated actions

--- client/src/SocketActions.ts
+++ client/src/SocketActions.ts
@@ -1,6 +1,7 @@
 export enum ESocketActions {
   CONNECTION = 'connection',
   JOIN = 'join',
+  JOINED = 'joined',
   LEAVE = 'leave',
   ON_CODE_CHANGE = 'on code change',
   DISCONNECT = 'disconnect',
---
+++ server/src/SocketActions.ts
@@ -0,0 +1,8 @@
+export enum ESocketActions {
+  CONNECTION = 'connection',
+  JOIN = 'join',
+  JOINED = 'joined',
+  LEAVE = 'leave',
+  ON_CODE_CHANGE = 'on code change',
+  DISCONNECT = 'disconnect',
+}
