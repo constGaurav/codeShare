2222e3497c298f89ec3b50ae163b51a33695c491
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2024-01-09 23:17:43 +0530

    added socket.io in backend

---
+++ SocketActions.ts
@@ -0,0 +1,7 @@
+export enum ESocketActions {
+  CONNECTION = 'connection',
+  JOIN = 'join',
+  LEAVE = 'leave',
+  ON_CODE_CHANGE = 'on code change',
+  DISCONNECT = 'disconnect',
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,37 @@
 import express, { Request, Response } from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import { ESocketActions } from '../../SocketActions';
+
+import env from 'dotenv';
+env.config();
 
 const app = express();
-const PORT = 3001;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+io.on(ESocketActions.CONNECTION, (socket: Socket) => {
+  console.log('a user connected', socket.id);
+  // On Join
+  socket.on(ESocketActions.JOIN, () => {
+    console.log('user joined');
+  });
+
+  socket.on(ESocketActions.ON_CODE_CHANGE, (data) => {
+    io.emit(ESocketActions.ON_CODE_CHANGE, data);
+  });
+
+  // On Disconnect
+  socket.on(ESocketActions.DISCONNECT, () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4001;
+server.listen(PORT, () => {
+  console.log('Listening at PORT', PORT);
 });
